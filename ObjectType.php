faacaf03b71b0c875b644b648c5093ab6f884e3f
Alex <admin@devblog.pro>
2016-02-20 17:35:03 +0500

    initial

---
+++ ObjectType.php
@@ -0,0 +1,33 @@
+<?
+namespace Kladr;
+
+/**
+ * Перечисление типов объектов
+ */
+class ObjectType
+{
+	/**
+	 * Регион
+	 */
+	const Region = 'region';
+
+	/**
+	 * Район
+	 */
+	const District = 'district';
+
+	/**
+	 * Населённый пункт
+	 */
+	const City = 'city';
+
+	/**
+	 * Улица
+	 */
+	const Street = 'street';
+
+	/**
+	 * Строение
+	 */
+	const Building = 'building';
+}
